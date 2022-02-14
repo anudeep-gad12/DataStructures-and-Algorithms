8e9cd9a95b45d36fd25744138566b28fd2fb942f
Anudeep <anudeep.gad@gmail.com>
2022-02-14 13:25:55 +0530

    Added Queue Problems

---
+++ Queue/Implementation.js
@@ -0,0 +1,50 @@
+// Implementation of Queue using LinkedList
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // Add a node to the queue (at the end of the queue)
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  // Remove a node from the beginning of the queue
+
+  dequeue() {
+    if (!this.first) return;
+    let temp = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.size--;
+    return temp.val;
+  }
+}
+
+const queue = new Queue();
+console.log(queue);
